2ec5fb39f297f462649b8571318d93a70b98a6d3
CHEVULA SOMESH <146305649+Somesh2504@users.noreply.github.com>
2024-12-30 22:42:49 +0530

    Update DataBaseServeer.js

--- FoodServer/DataBaseServeer.js
+++ FoodServer/DataBaseServeer.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 const connection = mysql.createConnection(
     {
-        host: process.env.DB_HOST || 'localhost',
-        user: process.env.DB_USER || 'root',
-        password: process.env.DB_PASSWORD || 'Somesh@2504',
-        database: process.env.DB_NAME || 'fooditems',
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD ,
+        database: process.env.DB_NAME,
     }
 );
 
