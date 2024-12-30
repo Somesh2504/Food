0c270fa1861f0b4fe72a263d86dfc89aa8f62249
Somesh2504 <someshchevula25@gmail.com>
2024-12-30 20:48:28 +0530

    changing dbserver

--- FoodServer/DataBaseServeer.js
+++ FoodServer/DataBaseServeer.js
@@ -1,40 +1,37 @@
-const db=require('mysql2');
-const connection=db.createConnection(
+const mysql = require('mysql2');
+const connection = mysql.createConnection(
     {
-        host:'localhost',
-        user:'root',
-        password:'Somesh@2504',
-        database:'fooditems',
-
+        host: process.env.DB_HOST || 'localhost',
+        user: process.env.DB_USER || 'root',
+        password: process.env.DB_PASSWORD || 'Somesh@2504',
+        database: process.env.DB_NAME || 'fooditems',
     }
-)
+);
 
-function getData(){
-   return new Promise((success,reject)=>{
-    connection.query('select * from fooditems',(err,rows,cols)=>{
-        if(err){
-           reject(err);
-        }
-        else{
-           success(rows);
-        }
+function getData() {
+    return new Promise((success, reject) => {
+        connection.query('SELECT * FROM fooditems', (err, rows, cols) => {
+            if (err) {
+                reject(err);
+            } else {
+                success(rows);
+            }
+        });
     });
-   
-   });
 }
+
 function postData(name, img, price, description) {
-   return new Promise((success,reject)=>{
-    const query = 'INSERT INTO fooditems (item_name, img, price, description) VALUES (?, ?, ?, ?)';
-    const values = [name, img, price, description];
-    connection.query(query, values, (err, rows) => {
-        if (err) {
-           reject(err);
-        } else {
-           success(rows);
-        }
+    return new Promise((success, reject) => {
+        const query = 'INSERT INTO fooditems (item_name, img, price, description) VALUES (?, ?, ?, ?)';
+        const values = [name, img, price, description];
+        connection.query(query, values, (err, rows) => {
+            if (err) {
+                reject(err);
+            } else {
+                success(rows);
+            }
+        });
     });
-    
-   })
 }
 
-module.exports={getData,postData};
+module.exports = { getData, postData };
