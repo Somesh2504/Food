cfa3ce9ff9a38b7282f47c263d0a5e0b19a3d73e
Somesh2504 <someshchevula25@gmail.com>
2024-12-31 08:22:12 +0530

    commrnt

--- FoodServer/DataBaseServeer.js
+++ FoodServer/DataBaseServeer.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 const connection = mysql.createConnection(
     {
-        host: process.env.DB_HOST || 'localhost',
+        host: process.env.DB_HOST || 'jdbc:mysql://localhost:3306/fooditems',
         user: process.env.DB_USER || 'root',
         password: process.env.DB_PASSWORD || 'Somesh@2504',
         database: process.env.DB_NAME || 'fooditems',
